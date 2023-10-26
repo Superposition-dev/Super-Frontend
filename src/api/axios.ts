717b7f4444fe4e2f074ed60a707b167279cf8e4b
Nevacat <nevacattery217@naver.com>
2023-10-26 16:52:48 +0900

    feat: msw 초기값 셋팅

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 
 const getAxiosInstance = (ContentType: string) => {
   const config: AxiosRequestConfig = {
-    baseURL: 'localhost:3000',
+    baseURL: 'localhost:5173',
     headers: {
       'Content-Type': ContentType,
     },
