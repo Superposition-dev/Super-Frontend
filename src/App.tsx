946fc5939e7db24e6aeeaa924fe3af715b56469c
HocaHeesu <heesu@hoca.dev>
2023-10-28 23:29:25 +0900

    feat: 레이아웃 적용 및 reset css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import './App.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { worker } from './mock/browser';
+import Layout from './components/common/Layout';
+import './styles/reset.css';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -14,7 +15,11 @@ if (import.meta.env.MODE === 'development') {
 }
 
 function App() {
-  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+  return <QueryClientProvider client={queryClient}>
+    <Layout>
+      <div>App</div>
+    </Layout>
+  </QueryClientProvider>;
 }
 
 export default App;
