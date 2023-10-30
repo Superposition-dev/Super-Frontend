af42983d6a7d163821662ec67ac50d9495b9b015
Tteum00 <wjdtmddnjs0913@naver.com>
2023-10-30 16:45:54 +0900

    feat: 파일생성 및 라우터 생성

---
+++ src/components/Detail/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Detail() {
+  return (
+    <div>
+      아아아아아아아
+    </div>
+  )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from '../components/common/Layout'
 import MainPage from '../pages/main'
+import Detail from '../components/Detail'
 
 function Routers() {
   return (
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route index element={<MainPage/>}/>
+        <Route path='/detail' element={<Detail/>}></Route>
       </Route>
     </Routes>
   )
