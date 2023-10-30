ebb10b0cb16b3cec5fd81f82caaa10956946110b
Nevacat <nevacattery217@naver.com>
2023-10-30 09:25:47 +0900

    styles: 폰트 변경
    pretendard 설정

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,22 +1,20 @@
-import { Global, css } from '@emotion/react'
+import { Global, css } from '@emotion/react';
 
 const style = css`
-  :root{
+  :root {
     font-size: 10px;
     font-weight: 400;
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Pretendard-Regular', sans-serif;
     color: #45537c;
   }
-  body{
+  body {
     height: 100vh;
     background-color: #f2f2f2;
   }
-`
+`;
 
 function GlobalStyles() {
-  return (
-    <Global styles={style}/>
-  )
+  return <Global styles={style} />;
 }
 
-export default GlobalStyles
+export default GlobalStyles;
