22d996aaac3b293ac70e132929d74dc202ba4b45
Nevacat <nevacattery217@naver.com>
2023-10-30 09:27:04 +0900

    style: theme 스타일 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,44 @@
+const colors = {
+  purple: '#6e28ac',
+  white: '#fefefe',
+  black: '#121212',
+  gray: '#797979',
+  lightGray: '#b8b8b8',
+};
+
+const font = {
+  mobile: {
+    b20: {
+      fontSize: '2rem',
+      fontWeight: '700',
+      lineHeight: '1.2',
+    },
+    r14: {
+      fontSize: '1.4rem',
+      fontWeight: '400',
+      lineHeight: '1.2',
+    },
+    r12: {
+      fontSize: '1.2rem',
+      fontWeight: '400',
+      lineHeight: '1.4',
+    },
+    b12: {
+      fontSize: '1.2rem',
+      fontWeight: '700',
+      lineHeight: '1.4',
+    },
+    b10: {
+      fontSize: '1rem',
+      fontWeight: '700',
+      lineHeight: '1.4',
+    },
+  },
+};
+
+const theme = {
+  colors,
+  font,
+};
+
+export default theme;
