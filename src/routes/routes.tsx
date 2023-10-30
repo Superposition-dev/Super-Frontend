1c5126d2fcd252f335f6a5f826d99666e4d1789d
Nevacat <nevacattery217@naver.com>
2023-10-30 16:17:55 +0900

    feat: 작품목록 라우트 추가

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from '../components/common/Layout'
-import MainPage from '../pages/main'
+import { Routes, Route } from 'react-router-dom';
+import Layout from '../components/common/Layout';
+import MainPage from '../pages/main';
+import ProductsPage from '../pages/products';
 
 function Routers() {
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<MainPage/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/products" element={<ProductsPage />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default Routers
+export default Routers;
