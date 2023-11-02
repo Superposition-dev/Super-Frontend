c60ebb3e2e22e34ed9ccf684da883aff571a83c7
Nevacat <nevacattery217@naver.com>
2023-11-02 14:01:55 +0900

    Refactor:작품 클릭 시 상세정보 연결

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,18 +1,16 @@
-
-import { Routes, Route } from 'react-router-dom'
-import Layout from '../components/common/Layout'
-import MainPage from '../pages/main'
-import Detail from '../components/Detail'
+import { Routes, Route } from 'react-router-dom';
+import Layout from '../components/common/Layout';
+import MainPage from '../pages/main';
+import Detail from '../components/Detail';
 import ProductsPage from '../pages/products';
-
 
 function Routers() {
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<MainPage/>}/>
-        <Route path='/detail' element={<Detail/>}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<Detail />} />
       </Route>
     </Routes>
   );
