285619f1b76be6a3a11cfeef43b39ca723a4354a
Nevacat <nevacattery217@naver.com>
2023-11-02 14:05:19 +0900

    Design:작품목록 글자 간격 수정에 따른 폰트 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -18,6 +18,16 @@ const font = {
       fontWeight: '700',
       lineHeight: '1.2',
     },
+    r18: {
+      fontSize: '1.8rem',
+      fontWeight: '400',
+      lineHeight: '1.2',
+    },
+    r16: {
+      fontSize: '1.6rem',
+      fontWeight: '400',
+      lineHeight: '1.2',
+    },
     r14: {
       fontSize: '1.4rem',
       fontWeight: '400',
