48fdb9d512d99532810f09a0ad3a44c426b49390
Nevacat <nevacattery217@naver.com>
2023-11-03 17:35:16 +0900

    feat: 좋아요 응답 값 변경:

--- src/mock/PostHandler.ts
+++ src/mock/PostHandler.ts
@@ -26,12 +26,16 @@ export const PostHandler = [
   rest.patch('/products/:id/like', async (req, res, ctx) => {
     const id = req.params.productId;
     const body = await req.json();
+    console.log(body);
     const { isLike } = body;
     const product = PostsData.filter((product) => product.productId === Number(id));
     if (!product) {
       return res(ctx.status(404), ctx.json({ message: '작품이 존재하지 않습니다.' }));
     }
 
-    return res(ctx.status(200), ctx.json(isLike ? { message: '좋아요 완료' } : { message: '좋아요 취소' }));
+    return res(
+      ctx.status(200),
+      ctx.json(isLike ? { data: 'true', message: '좋아요 완료' } : { data: 'false', message: '좋아요 취소' })
+    );
   }),
 ];
