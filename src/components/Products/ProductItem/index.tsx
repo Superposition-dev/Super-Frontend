04c2918f1dabf4576effa200b5ee5b0c75b46776
HocaHeesu <heesu@hoca.dev>
2023-11-04 22:10:28 +0900

    fix:코드 요청값에 따른 개별 목록 태그 수정

--- src/components/Products/ProductItem/index.tsx
+++ src/components/Products/ProductItem/index.tsx
@@ -1,18 +1,18 @@
 import * as S from './styles';
 /*eslint-disable */
 function ProductItem({ item }: any) {
-  const { picture, name, title, tag } = item;
+  const { picture, artist, title, tags } = item;
   return (
     <>
       <S.Image src={picture} alt={'test'} />
       <S.TagList>
-        {tag &&
-          tag.map((item: string) => {
+        {tags &&
+          tags.map((item: string) => {
             return <S.Tag key={item}>#{item}</S.Tag>;
           })}
       </S.TagList>
       <S.ItemTitle>{title}</S.ItemTitle>
-      <S.Artist>{name}</S.Artist>
+      <S.Artist>{artist}</S.Artist>
     </>
   );
 }
