4b78acc28ee2dfd30c7ae8c181838fada99a1ba5
Nevacat <nevacattery217@naver.com>
2023-11-06 17:39:47 +0900

    feat: 작품 검색 기능 추가

--- src/api/postsAPI.ts
+++ src/api/postsAPI.ts
@@ -1,22 +1,23 @@
 import { axiosInstance } from './axios';
 
-export const getPosts = async () => {
-  const res = await axiosInstance.get('/products');
-  console.log(res.data)
+export const getPosts = async (filter: string | null | undefined) => {
+  const res = await axiosInstance.get(filter ? `/products?filter=${filter}` : '/products');
+  console.log(res.data);
   return res.data;
 };
 
 export const getPost = async (id: number, qr: boolean) => {
   const res = await axiosInstance.get(`/products/${id}?qr=${qr === null ? true : qr}`);
+  console.log(res.data);
   return res.data;
 };
 
-type Props ={
-  id: number,
-  like: boolean
-}
+type Props = {
+  id: number;
+  like: boolean;
+};
 
-export const likePost = async ({id,like}:Props) => {
-  const res = await axiosInstance.patch(`/products/${id}/like`,{like});
+export const likePost = async ({ id, like }: Props) => {
+  const res = await axiosInstance.patch(`/products/${id}/like`, { like });
   return res.data;
 };
--- src/components/Products/styles.ts
+++ src/components/Products/styles.ts
@@ -1,9 +1,19 @@
 import styled from '@emotion/styled';
 import theme from '../../styles/theme';
 import { Link } from 'react-router-dom';
+import { BiSearch } from 'react-icons/bi';
 
 export const Container = styled.div`
+  width: 100%;
   padding: 0 1rem;
+`;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem 0 0.5rem;
+  position: relative;
 `;
 
 export const Title = styled.h1`
@@ -11,7 +21,45 @@ export const Title = styled.h1`
   font-family: 'Lemon', serif;
   color: ${theme.colors.white};
   text-align: center;
-  margin: 1rem 0 2rem;
+`;
+
+export const Search = styled(BiSearch)`
+  color: ${theme.colors.white};
+  font-size: 2.6rem;
+  position: absolute;
+  right: 1.5rem;
+`;
+
+export const SearchWrap = styled.form<{ isSearch: boolean }>`
+  display: flex;
+  height: ${(props) => (props.isSearch ? '3rem' : '0')};
+  visibility: ${(props) => (props.isSearch ? 'visible' : 'hidden')};
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1rem;
+  transition: all 0.3s ease-in-out;
+  position: relative;
+`;
+
+export const SearchInput = styled.input`
+  width: 90%;
+  height: 100%;
+  color: ${theme.colors.black};
+  margin: 0 auto;
+  padding: 0 0.8rem;
+  border-radius: 0.5rem;
+  border: none;
+  outline: none;
+`;
+
+export const Button = styled.button`
+  position: absolute;
+  right: 2rem;
+  border: none;
+  background-color: transparent;
+  font-weight: 700;
+  cursor: pointer;
+  color: ${theme.colors.black};
 `;
 
 export const ProductItem = styled(Link)`
@@ -20,7 +68,7 @@ export const ProductItem = styled(Link)`
   cursor: pointer;
   width: calc(20% - 1.6rem);
   height: auto;
-  @media (max-width: 420px) {
+  @media (max-width: 428px) {
     width: calc(50% - 1.6rem);
   }
 `;
