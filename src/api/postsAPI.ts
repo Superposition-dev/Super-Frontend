db9cd9468bcde24c7a0e07307f563aded301903f
Nevacat <nevacattery217@naver.com>
2023-11-07 13:28:07 +0900

    Docs:코드 위치 수정

--- src/api/postsAPI.ts
+++ src/api/postsAPI.ts
@@ -1,19 +1,14 @@
 import { axiosInstance } from './axios';
 
-
-
-export const getPost = async (id: number, qr: string) => {
-  const res = await axiosInstance.get(`/products/${id}?isQr=${qr === "false" ? false : true}`);
-  console.log(res.data);
-  return res.data;
-};
-
 export const getPosts = async (filter: string | null | undefined) => {
   const res = await axiosInstance.get(filter ? `/products?filter=${filter}` : '/products');
   return res.data;
 };
 
-
+export const getPost = async (id: number, qr: string) => {
+  const res = await axiosInstance.get(`/products/${id}?isQr=${qr === 'false' ? false : true}`);
+  return res.data;
+};
 
 type Props = {
   id: number;
