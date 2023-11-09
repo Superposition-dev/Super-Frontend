b80930c5478d6e624eebfa3ce882c6a11ac23063
Nevacat <nevacattery217@naver.com>
2023-11-09 23:55:15 +0900

    fix: 버그 수정 및 레이아웃 재구성

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -100,8 +100,8 @@ export default function Detail() {
                   </S.icons>
                 </S.Anrdma>
                 <S.Tags>
-                  {products.tags &&
-                    products.tags.map((item: string) => {
+                  {products.tag &&
+                    products.tag.map((item: string) => {
                       return <S.Tag key={item}>#{item}</S.Tag>;
                     })}
                 </S.Tags>
